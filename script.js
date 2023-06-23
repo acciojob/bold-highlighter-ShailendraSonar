3fb358b61c4cf6866aaaa96b1296413e0365892a
Shailendra Sonar <77749654+ShailendraSonar@users.noreply.github.com>
2023-06-23 15:00:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
     //Write your code here
 
+    var strong = document.getElementsByTagName('strong');
+    
+    for(var i in strong){
+        strong[i].style.color = 'green';
+    }
+
 
 }
 
 
 function return_normal() {
     //Write your code here
+   
 
+    var strong = document.getElementsByTagName('strong');
     
+    for(var i in strong){
+        strong[i].style.color = 'black';
+    }
+
 }
